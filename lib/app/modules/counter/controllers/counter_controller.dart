703b83c30d1640e5620c0ff4522365d18282d9b6
badyubayidillah <badyubayidillah@gmail.com>
2024-08-19 09:18:10 +0000

    getx

---
+++ lib/app/modules/counter/controllers/counter_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+  RxInt bilangan = 0.obs;
+
+  void tambahsatu() {
+    if (bilangan.value >= 20) {
+      Get.snackbar('Warning', 'loba teuing plok');
+    } else {
+      bilangan.value++;
+    }
+  }
+
+void kurangsatu() {
+    if (bilangan.value >= 0) {
+      Get.snackbar('Warning', 'saetik teuing plok');
+    } else {
+      bilangan.value++;
+    }
+  }
+}
